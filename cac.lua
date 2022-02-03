c2a7cdd6ec3d29f22ed4148dc8394463bffd58cf
X2Duc <97432322+X2Duc@users.noreply.github.com>
2022-02-03 20:52:19 +0700

    Create cac.lua

---
+++ cac.lua
@@ -0,0 +1,9 @@
+if game.PlaceId==2753915549 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/X2Duc/RxDHub/main/DitmeSea1"))()
+  end
+    if game.PlaceId==3102144307 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/X2Duc/duc/main/dsadada"))()
+  else 
+      game.Players.LocalPlayer:Kick("RxD Hub not supported this game!, please check at #support channel! | Copy link discord Invite!")
+        setclipboard("https://discord.gg/wS2aDbjzjy")
+end
